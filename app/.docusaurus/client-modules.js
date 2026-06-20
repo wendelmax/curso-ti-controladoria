981ebc612d9e13c457fb61859e6cba10b13b5213
Jackson Sá <jacksonwendel@gmail.com>
2026-06-20 12:15:27 -0300

    Remove .docusaurus cache from repo

--- app/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/mnt/c/Users/wende/Projects/curso_ti_controladoria/app/node_modules/infima/dist/css/default/default.css"),
-  require("/mnt/c/Users/wende/Projects/curso_ti_controladoria/app/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/mnt/c/Users/wende/Projects/curso_ti_controladoria/app/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/mnt/c/Users/wende/Projects/curso_ti_controladoria/app/src/css/custom.css"),
-];
